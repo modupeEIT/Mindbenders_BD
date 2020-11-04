512e37344e2d67541f8a2a93aff87c3c4fc7da27
modupeEIT <modupe.oyediran@enhanceit.us>
2020-11-04 11:58:50 -0500

    Task 3 in class

---
+++ hdfsBash.sh
@@ -0,0 +1,10 @@
+#!usr/bin/bash
+
+service=$(jps)
+if [[ "$service" == "$DataNode" ]]; then
+    echo "SERVICE RUNNING"
+else 
+    echo "SERVICE NOT RUNNING.... STARTING UP SERVICE " 
+	   eval "start-all.sh"
+    echo "SERVICE STARTED"
+fi
