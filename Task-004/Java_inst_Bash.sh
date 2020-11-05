9fa1e6451553efbebf6977f921d44d68be511ee6
modupeEIT <modupe.oyediran@enhanceit.us>
2020-11-05 08:14:33 -0500

    Task004

---
+++ Task-004/Java_inst_Bash.sh
@@ -0,0 +1,30 @@
+#!usr/bin/bash
+
+
+#check for update
+sudo apt update
+
+#create opt dir
+mkdir opt
+cd opt
+
+Download Java
+wget -O jdk-8u221-linux-x64.tar.gz \
+-c --content-disposition \
+"https://javadl.oracle.com/webapps/download/AutoDL?BundleId=239835_230deb18db3e4014bb8e3e8324f81b43"
+
+#Unzip
+tar -zxvf jdk-8u221-linux-x64.tar.gz
+
+#remove tar file
+rm jdk-8u221-linux-x64.tar.gz
+
+#home dir
+cd
+
+#set the path
+sudo gedit .bash_profile.sh
+echo "JAVA_HOME=/opt/jdk1.8.0_221" >> .bash_profile.sh
+echo "export PATH=$PATH:$JAVA_HOME/bin" >> .bash_profile.sh
+
+source .bash_profile.sh
